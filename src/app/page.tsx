d74ee4bffada7af92c9dcdd67d05f9e2b39ee727
lesley ayemi <leeayemi@gmail.com>
2025-09-07 01:30:48 +0100

    fixing company color and icon for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Portfolio() {
         <ExperienceSection />
 
         {/* Projects Section */}
-        <ProjectsSection />
+        {/* <ProjectsSection /> */}
 
         {/* Testimonials Section */}
         {/* <TestimonialsSection /> */}
